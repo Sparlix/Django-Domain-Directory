19f082cf50cc44774c0724480d20cb8637c302b3
LyfeTyme <48641165+LyfeTyme@users.noreply.github.com>
2020-06-13 22:45:02 -0600

    mvp version 1.0

--- domain/models.py
+++ domain/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Domain(models.Model):
+    # DOMAIN NAME
+    domain_Name = models.CharField(max_length=30)
+    # LINK TO REGISTRAR
+    domain_Registrar = models.CharField(max_length=30)
+    # NAME OF PERSON WHO OWNS IT
+    domain_Owner = models.CharField(max_length=30)
+    #DATE OF EXPIRY
+    domain_Expiration = models.DateField()
+    #PRICE TO RENEW
+    domain_Renew_Fee = models.DecimalField(max_digits=6, decimal_places=2)
+    # YEARLY OR MONHTLY 
+    domain_Renew_TimePeriod = models.CharField(max_length=10)
+
+    domain_Link = models.CharField(max_length=30)
+
+class Contact(models.Model):
+    email = models.CharField(max_length=30)
--- domain/views.py
+++ domain/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Domain
+from .models import Contact
 
 
 def home(request):
-    return render(request, 'home.html', {})
+    domain = Domain.objects
+    return render(request, 'home.html', {'domain': domain})
+
+def contact(request):
+    contact = Contact.objects
+    return render(request,'contact.html', {'contact': contact})
 # Create your views here.
